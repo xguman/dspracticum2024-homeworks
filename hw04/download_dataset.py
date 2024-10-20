97e3a0b4bdd0c8ae34cd9fce285a855c7fc6ecca
peterguman <492964@mail.muni.cz>
2024-10-20 11:01:20 +0200

    download randomly 500 pictures from dataset

--- hw04/download_dataset.py
+++ hw04/download_dataset.py
@@ -1,21 +1,23 @@
 import os
 from datasets import load_dataset
 from PIL import Image
+import random
 
 def download_images(name="food101"):
-    dataset = load_dataset(name, split="train[:1%]")  # Get a small percentage of the data
+    dataset = load_dataset(name, split="train")
 
+    # Create a directory to save the images if it doesn't exist
     output_dir = "data/pictures"
     os.makedirs(output_dir, exist_ok=True)
 
-    num_images = 300
-    count = 0
-
-    for example in dataset:
-        if count >= num_images:
-            break
+    # Limit to 500 images
+    num_images = 500
+    
+    # Randomly select and save the images
+    random_indices = random.sample(range(len(dataset)), num_images)
+    for index in random_indices:
+        example = dataset[index]
         image = example['image']
         image.save(os.path.join(output_dir, f"image_{count}.jpg"))  # Save as JPG
-        count += 1
 
-    print(f"Downloaded and saved {count} images to the folder '{output_dir}'")
+    print(f"Downloaded and saved {num_images} images to the folder '{output_dir}'")
